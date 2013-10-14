fe99f8e8aea9115fd2e95c23e302ed28028a144e
Quasar Jarosz <quasar@ja.rosz.org>
2013-10-14 17:15:30 -0500

    added logo display when not logged in

--- tube/views.py
+++ tube/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.decorators import login_required
 from tube.models import Video, VideoForm
 
 # Create your views here.
-@login_required
 def index(request):
-    videos = Video.objects.all()
-    return render(request, 'index.html', {'videos':videos})
+    if request.user.is_authenticated():
+        videos = Video.objects.all()
+        return render(request, 'index.html', {'videos':videos})
+    else:
+        return render(request, 'logo.html')
 
 def video(request, name):
     try:
@@ -15,7 +17,7 @@ def video(request, name):
         try:
             video = Video.objects.get(hidden=name)
         except:
-            return HttpResponse("<html><body>No video found :(</body></html>")
+            return redirect(index)
     print("found video: {}".format(video.vid.url))
     return render(request, 'video.html', {'video':video})
 
